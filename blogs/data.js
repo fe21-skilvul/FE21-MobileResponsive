a538e0fffd7ee77a06b063354000ddde4922ec33
San Prastiwa <san.prastiwa@gmail.com>
2023-05-05 14:21:09 +0700

    add data.js

--- blogs/data.js
+++ blogs/data.js
@@ -4,39 +4,84 @@ const urlPost = "https://64527065a2860c9ed40c6783.mockapi.io/comments/";
 const body = [
   {
     image:
-      "https://skilvul-prod-01.s3.ap-southeast-1.amazonaws.com/blog/raih-pekerjaan-impian-di-tengah-keterbatasan-fisik-details-1678783692640.jpg",
-    artikel: `Mega Kuningan di Jakarta memang terkenal sebagai kawasan yang cukup ramai dengan pusat perbelanjaan maupun perkantoran. Meskipun padat dan seringkali terjadi kemacetan, bagi Dolos bisa bekerja di salah satu kantor di daerah tersebut merupakan salah satu impian yang akhirnya bisa terwujud. Setelah mendapatkan beasiswa Difabel Bisa Ngoding yang diselenggarakan Skilvul bersama Telkom Indonesia Divisi Wholesale Service (Telkom DWS), akhirnya Dolos bisa mendapatkan kesempatan meraih pekerjaan impian di tengah keterbatasan dirinya.
+      "https://cdn.idntimes.com/content-images/post/20221111/pexels-alvaro-camacho-13153905-440057c5400f512165c654b2903400dd_600x400.jpg",
+    artikel: `Melihat patung memang memberikan sensasi tersendiri. Ada yang takut, kagum, dan penasaran mengenai patung tersebut. Ada patung yang menyerupai bentuk manusia, hewan, bahkan tumbuhan.
 
-Habell Dolosha Purba atau yang biasa dipanggil Dolos, merupakan lulusan S1 Sistem Informasi di Universitas Klabat, Manado. Dolos memiliki keterbatasan yaitu tuna daksa, di mana tangannya kurang bisa memegang sesuatu dengan baik dan normal. Selain itu kakinya juga sedikit memiliki keterbatasan gerak meskipun masih bisa berdiri dan berjalan. Sebagai anak pertama dari tiga bersaudara, tentunya keterbatasan diri yang dimilikinya tidak serta merta mematahkan semangat dirinya. Meskipun semasa perkuliahan Dolos sempat mengalami kesulitan dalam kegiatan pembelajaran, salah satunya saat diminta untuk mengetik cepat pada saat ujian mata kuliah kemampuan dasar komputer, hal itu tidak membuat Dolos berhenti berusaha.
+    Patung-patung yang mengimitasi bentuk-bentuk makhluk yang ada di dunia nyata itu, merupakan patung figuratif. Para pengrajin patung atau pematung akan membuat mahakarya dengan utuh sesuai dengan replika aslinya. Untuk lebih lanjut, mari mengenal pengertian patung figuratif.
+    
+    Apa itu patung Figuratif
+    
+    Melansir buku Diksi Rupa, Kumpulan Istilah dan Gerakan Seni Rupa (2011), patung figuratif merupakan jenis patung yang menginterpretasikan kata “figur”, memiliki makna objek yang terbentuk, serta mempunyai kesamaan dengan suatu bentuk tertentu. Patung figuratif dapat berbentuk makhluk hidup seperti manusia, hewan, tumbuhan, serta beberapa objek lainnya.
 
-Perjuangan Dolos tidak berhenti sampai di situ, setelah lulus kuliah pada Februari 2021, dirinya harus menerima kenyataan pahit tidak diterima di berbagai kesempatan kerja yang dia impikan. Selama kurang lebih setahun, Dolos masih belum mendapatkan pekerjaan hingga akhirnya dia melihat kesempatan untuk meningkatkan kemampuan digital melalui program beasiswa Difabel Bisa Ngoding yang diselenggarakan Skilvul bersama Telkom DWS. Dolos merasa selama mengikuti program beasiswa Difabel Bisa Ngoding, dirinya akhirnya bisa mendapatkan learning path yang tersusun dengan baik. Selain itu, para mentor yang sangat suportif selama proses pembelajaran menjadikan dirinya selalu tertantang dan bersemangat menyelesaikan tugas-tugas yang diberikan.
-
-Bagi Dolos, salah satu materi soft skill paling berkesan saat mengikuti program Difabel Bisa Ngoding adalah mengenai membangun mentalitas saat pencarian kerja. Teringat akan pengalamannya yang hampir putus asa saat mencari kerja selama kurang lebih setahun, Dolos sadar bahwa kegagalan itu bukan sesuatu yang harus ditakuti. Hal terpenting sebenarnya adalah tidak pernah berhenti untuk mencoba. Sekarang dirinya merasa bisa lebih siap dan bijak dalam menghadapi kegagalan. Berkat semangatnya yang tidak pernah putus meskipun di tengah keterbatasan, akhirnya Dolos bisa diterima bekerja di salah satu bank swasta sebagai Management Trainee di bidang IT.`,
-    title: "1. Raih Pekerjaan Impian di Tengah Keterbatasan Fisik",
+Dalam penggambaran patung figuratif, pematung biasanya menambahkan ciri khas estetika dari seni pop-surealis sehingga karya-karyanya lebih dramatis dan memiliki nilai seni yang tinggi. Hal ini menjadi ciri khas dari patung figuratif dibandingkan patung non-figuratif.`,
+    title: "1. Mengenal Pengertian Patung Figuratif, Ciri-ciri, dan Contoh",
   },
   {
     image:
-      "https://skilvul-prod-01.s3.ap-southeast-1.amazonaws.com/blog/raih-pekerjaan-impian-di-tengah-keterbatasan-fisik-details-1678783692640.jpg",
-    artikel: `Daerah Mega Kuningan di Jakarta memang terkenal sebagai kawasan yang cukup ramai dengan pusat perbelanjaan maupun perkantoran. Meskipun padat dan seringkali terjadi kemacetan, bagi Dolos bisa bekerja di salah satu kantor di daerah tersebut merupakan salah satu impian yang akhirnya bisa terwujud. Setelah mendapatkan beasiswa Difabel Bisa Ngoding yang diselenggarakan Skilvul bersama Telkom Indonesia Divisi Wholesale Service (Telkom DWS), akhirnya Dolos bisa mendapatkan kesempatan meraih pekerjaan impian di tengah keterbatasan dirinya.
+      "https://cdn.idntimes.com/content-images/post/20230315/nirmana1-e1637812423436-02f9abd64717a77b4d47e0c7bf13be90_600x400.jpg",
+    artikel: `Pernahkah kamu mendengar tentang kata nirmana? Terlebih, nirmana adalah salah satu mata kuliah yang sangat fenomenal di kalangan anak seni, terutama Desain Komunikasi Visual (DKV), nih.
 
-Habell Dolosha Purba atau yang biasa dipanggil Dolos, merupakan lulusan S1 Sistem Informasi di Universitas Klabat, Manado. Dolos memiliki keterbatasan yaitu tuna daksa, di mana tangannya kurang bisa memegang sesuatu dengan baik dan normal. Selain itu kakinya juga sedikit memiliki keterbatasan gerak meskipun masih bisa berdiri dan berjalan. Sebagai anak pertama dari tiga bersaudara, tentunya keterbatasan diri yang dimilikinya tidak serta merta mematahkan semangat dirinya. Meskipun semasa perkuliahan Dolos sempat mengalami kesulitan dalam kegiatan pembelajaran, salah satunya saat diminta untuk mengetik cepat pada saat ujian mata kuliah kemampuan dasar komputer, hal itu tidak membuat Dolos berhenti berusaha.
+    Nah, sebagai mahasiswa DKV, tentu saja nirmana menjadi mata kuliah dasar yang wajib ditempuh. Mata kuliah ini biasanya akan diletakkan pada semester awal perkuliahan.
+    
+    Namun, tahukah kamu apa itu nirmana? Bagi yang penasaran, berikut IDN Times berikan penjelasan dari nirmana.
+    
+    Apa Itu Nirmana?
+    
+    Nirmana adalah sebuah ilmu tata rupa yang memiliki pengaruh yang begitu besar kepada sebuah desain. Secara kontekstual, nirmana memiliki arti asas atau prinsip yang menjadi pegangan atau patokan bagi seniman saat merancang.
 
-Perjuangan Dolos tidak berhenti sampai di situ, setelah lulus kuliah pada Februari 2021, dirinya harus menerima kenyataan pahit tidak diterima di berbagai kesempatan kerja yang dia impikan. Selama kurang lebih setahun, Dolos masih belum mendapatkan pekerjaan hingga akhirnya dia melihat kesempatan untuk meningkatkan kemampuan digital melalui program beasiswa Difabel Bisa Ngoding yang diselenggarakan Skilvul bersama Telkom DWS. Dolos merasa selama mengikuti program beasiswa Difabel Bisa Ngoding, dirinya akhirnya bisa mendapatkan learning path yang tersusun dengan baik. Selain itu, para mentor yang sangat suportif selama proses pembelajaran menjadikan dirinya selalu tertantang dan bersemangat menyelesaikan tugas-tugas yang diberikan.
+Istilah nirmana sendiri lahir dari gabungan antara dua kata, yaitu ‘nir’ yang artinya ‘tidak ada’, dan 'mana’ yang berarti ‘makna’. Dengan demikian, bisa ditarik sebuah kesimpulan bahwa nirmana adalah ‘sesuatu yang tidak bermakna’.
 
-Bagi Dolos, salah satu materi soft skill paling berkesan saat mengikuti program Difabel Bisa Ngoding adalah mengenai membangun mentalitas saat pencarian kerja. Teringat akan pengalamannya yang hampir putus asa saat mencari kerja selama kurang lebih setahun, Dolos sadar bahwa kegagalan itu bukan sesuatu yang harus ditakuti. Hal terpenting sebenarnya adalah tidak pernah berhenti untuk mencoba. Sekarang dirinya merasa bisa lebih siap dan bijak dalam menghadapi kegagalan. Berkat semangatnya yang tidak pernah putus meskipun di tengah keterbatasan, akhirnya Dolos bisa diterima bekerja di salah satu bank swasta sebagai Management Trainee di bidang IT.`,
-    title: "2. Raih Pekerjaan Impian di Tengah Keterbatasan Fisik",
+Artinya, ‘tanpa makna’ merujuk pada hasil akhir dari karya seni yang tak boleh membentuk sesuatu yang jelas, seperti simbol, angka, logo, binatang, atau benda. Tetapi, nirmana bisa dirasakan, seperti mengetahui apakah karya tersebut kasar, lembut, hingga dinamis.`,
+    title: "2. Nirmana Adalah Ilmu Tata Rupa, Calon Anak DKV Wajib Tahu!",
   },
   {
     image:
-      "https://skilvul-prod-01.s3.ap-southeast-1.amazonaws.com/blog/raih-pekerjaan-impian-di-tengah-keterbatasan-fisik-details-1678783692640.jpg",
-    artikel: `Daerah Mega Kuningan di Jakarta memang terkenal sebagai kawasan yang cukup ramai dengan pusat perbelanjaan maupun perkantoran. Meskipun padat dan seringkali terjadi kemacetan, bagi Dolos bisa bekerja di salah satu kantor di daerah tersebut merupakan salah satu impian yang akhirnya bisa terwujud. Setelah mendapatkan beasiswa Difabel Bisa Ngoding yang diselenggarakan Skilvul bersama Telkom Indonesia Divisi Wholesale Service (Telkom DWS), akhirnya Dolos bisa mendapatkan kesempatan meraih pekerjaan impian di tengah keterbatasan dirinya.
+      "https://cdn.idntimes.com/content-images/post/20230205/img-20230203-155006-822f8e69571a8803ac0359764d638505_600x400.jpg",
+    artikel: `Jalan Braga merupakan salah satu tempat favorit wisatawan ketika berkunjung ke Kota Bandung. Bangunan yang masih bergaya Eropa yang memanjakan mata dan beragam kuliner yang ada di tiap sudut menjadikan kawasan ini ramai dikunjungi.
 
-Habell Dolosha Purba atau yang biasa dipanggil Dolos, merupakan lulusan S1 Sistem Informasi di Universitas Klabat, Manado. Dolos memiliki keterbatasan yaitu tuna daksa, di mana tangannya kurang bisa memegang sesuatu dengan baik dan normal. Selain itu kakinya juga sedikit memiliki keterbatasan gerak meskipun masih bisa berdiri dan berjalan. Sebagai anak pertama dari tiga bersaudara, tentunya keterbatasan diri yang dimilikinya tidak serta merta mematahkan semangat dirinya. Meskipun semasa perkuliahan Dolos sempat mengalami kesulitan dalam kegiatan pembelajaran, salah satunya saat diminta untuk mengetik cepat pada saat ujian mata kuliah kemampuan dasar komputer, hal itu tidak membuat Dolos berhenti berusaha.
+    Tak hanya itu, Braga pun dikenal dengan keindahan lukisan-lukian yang dapat dinikmati di sepanjang jalan. Mulai dari lukisan yang dijual di pinggir jalan hingga yang ada di dalam galeri. Kawasan ini pun kemudian menjadi tempat anak muda membuat konten dengan foto-foto di area lukisan.
+    
+    Bagi wisatawan yang hendak berwisata ke Bandung dan mampir ke Jalan Braga, harus mencoba masuk Grey Art Gallery. Galeri seni ini baru saja dibuka secara resmi pada Jumat (3/2/2023). Lebih dari 100 karya seni pun dipamerkan di tempat tersebut.
+    
+    CEO Grey Art Gallery, Angga Aditya menuturkan, tujuan hadirnya galeri ini adalah untuk mendukung karier seniman Kota Bandung melalui kerja sama dan kolaborasi.
+    
+    Sebagai sebuah galeri, Grey Art Gallery tentu menghadirkan karya seni bagi masyarakat luas. Terdapat 115 karya yang ditampilkan. Beberapa karya berasal dari 17 seniman yang dipilih secara langsung melalui undangan.
+    
+    “Dari 850 karya, disaring jadi 115 karya melalui jalur open call. Sedangkan karya dari 17 seniman dipilih secara langsung melalui jalur undangan,” kata Angga, Minggu (5/2/2023).
+    
+    Beberapa karya yang ditampilkan merupakan karya dari Isa Perkasa, Prabu Perdana, dan Andreas Camelia dengan ciri khas masing-masing. Para pengunjung dapat menikmati karya dengan sensasi berbeda.
+    
+    “Kami percaya karya-karya yang ditampilkan, menjadi salah satu parameter artistik perkembangan seniman yang paling mutakhir di Kota Bandung,” ungkap Angga.`,
+    title:
+      "3. Grey Art Gallery, Tempat Baru di Jalan Braga yang Wajib Dikunjungi",
+  },
+  {
+    image:
+      "https://cdn.idntimes.com/content-images/post/20230316/contoh-aliran-surealisme-the-wounded-deer-frida-kahlo-serupa-id-583e82d05fbf80590251f0bbe792fd13_600x400.jpg",
+    artikel: `Seni lukis memiliki berbagai macam aliran yang menghasilkan lukisan yang indah dan bernilai estetika tinggi. Salah satunya adalah aliran Surealisme.
 
-Perjuangan Dolos tidak berhenti sampai di situ, setelah lulus kuliah pada Februari 2021, dirinya harus menerima kenyataan pahit tidak diterima di berbagai kesempatan kerja yang dia impikan. Selama kurang lebih setahun, Dolos masih belum mendapatkan pekerjaan hingga akhirnya dia melihat kesempatan untuk meningkatkan kemampuan digital melalui program beasiswa Difabel Bisa Ngoding yang diselenggarakan Skilvul bersama Telkom DWS. Dolos merasa selama mengikuti program beasiswa Difabel Bisa Ngoding, dirinya akhirnya bisa mendapatkan learning path yang tersusun dengan baik. Selain itu, para mentor yang sangat suportif selama proses pembelajaran menjadikan dirinya selalu tertantang dan bersemangat menyelesaikan tugas-tugas yang diberikan.
+    Aliran Surealisme merupakan aliran seni yang memperlihatkan gambar objek nyata tetapi tidak mungkin terjadi di dunia nyata. Aliran Surealisme juga memiliki citra seperti dalam mimpi. Agar mengetahui lebih lanjut, simak artikel berikut ini!
 
-Bagi Dolos, salah satu materi soft skill paling berkesan saat mengikuti program Difabel Bisa Ngoding adalah mengenai membangun mentalitas saat pencarian kerja. Teringat akan pengalamannya yang hampir putus asa saat mencari kerja selama kurang lebih setahun, Dolos sadar bahwa kegagalan itu bukan sesuatu yang harus ditakuti. Hal terpenting sebenarnya adalah tidak pernah berhenti untuk mencoba. Sekarang dirinya merasa bisa lebih siap dan bijak dalam menghadapi kegagalan. Berkat semangatnya yang tidak pernah putus meskipun di tengah keterbatasan, akhirnya Dolos bisa diterima bekerja di salah satu bank swasta sebagai Management Trainee di bidang IT.`,
-    title: "2. Raih Pekerjaan Impian di Tengah Keterbatasan Fisik",
+    Surealisme merupakan aliran yang menghadirkan kontradiksi antara mimpi dan realitas. Lalu, disajikan secara nyata ke dalam sebuah gambar yang menampilkan objek-objek yang nyata, tetapi dalam kondisi tidak akan terjadi di dunia nyata. Hal ini terdapat dalam imajinasi manusia. Aliran ini juga dinilai menggunakan pendekatan psikologi Freud yang mengeksplorasi alam bawah sadar mimpi dan citra manusia.
+
+    Alam bawah sadar manusia dapat menunjukkan imajinasi atau keinginan manusia yang sesungguhnya. Umumnya, tekanan sosial atau norma yang membuat imajinasi atau mimpi itu, terkubur di alam bawah sadar.
+    
+    Hasrat atau imajinasi tersebut tidak selalu berupa energi atau hal yang negatif. Hal ini dapat dicontohkan secara sederhana ketika mimpi masa kecil seseorang terus memudar karena menghadapi realita dan logika seiring beranjak dewasa. Oleh karena itu, surealisme dapat menyalurkan imajinasi tersebut dan merepresentasikan secara nyata. `,
+    title: "4. Mengenal Aliran Surealisme: Pengertian",
+  },
+  {
+    image:
+      "https://cdn.idntimes.com/content-images/post/20230403/pexels-sheep-1846422-636d72ceed4d351619273723b4fba94d_600x400.jpg",
+    artikel: `Keindahan merupakan kata sifat yang bisa datang dari mana pun. Keindahan bisa terdapat dari pemandangan alam, sebuah sudut rak buku di perpustakaan, hingga sekelompok kucing yang duduk di trotoar dapat disebut sebagai keindahan. 
+
+    Keindahan memiliki kata lain yakni estetika yang berarti erat kaitannya dengan suatu keindahan atau sesuatu yang berbau seni. Nah, untuk memahami makna dari estetika, simak informasi selengkapnya di bawah ini. 
+
+    Secara etimologis, istilah kata "estetika" berasal dari bahasa Yunani yang berarti "aestheticus" atau bahasa Yunani "aestheticos" yang berarti merasa. Secara etimologis pula estetika dapat ditangkap oleh panca indera manusia. 
+
+    Menurut Kamus Besar Bahasa Indonesia (KBBI), estetika merupakan cabang ilmu filsafat yang membahas tentang seni dan keindahan, juga tanggapan manusia tentang hal tersebut. Jika disederhanakan, estetika merupakan suatu kepekaan terhadap seni dan keindahan. 
+
+`,
+    title: "5. Estetika: Pengertian",
   },
 ];
 
